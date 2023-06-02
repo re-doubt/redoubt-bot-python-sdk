06a69b80841190f0b30c508944d4b4070310caf5
Maksym Nechepurenko <maksym.delta@protonmail.com>
2023-06-02 17:02:10 +0400

    endpoint fix

--- redoubt_agent/redoubt_sdk.py
+++ redoubt_agent/redoubt_sdk.py
@@ -12,7 +12,7 @@ from gql.transport.requests import RequestsHTTPTransport
 from gql.transport.websockets import WebsocketsTransport
 from loguru import logger
 
-from . import DEFAULT_ENDPOINT
+DEFAULT_ENDPOINT = 'ws://44.210.191.88:9080/v1/graphql'  # TODO update to wss
 
 
 class RedoubtEventsStream:
